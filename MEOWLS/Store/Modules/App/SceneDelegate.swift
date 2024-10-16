b53889866b654910833038eee2dca9c82dec94d6
Artem Mayer <mayer1art@gmail.com>
2024-10-16 21:17:02 +0700

    IOS-4 Add Intro view injection

--- MEOWLS/Store/Modules/App/SceneDelegate.swift
+++ MEOWLS/Store/Modules/App/SceneDelegate.swift
@@ -21,14 +21,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        setupFactory()
+
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIViewController()//resolve(\.introBuilder).build(with: .init())
+        window?.rootViewController = Router.introViewController()
         window?.makeKeyAndVisible()
         window?.overrideUserInterfaceStyle = .light
 
-        setupFactory()
         setupImageCache()
     }
 
